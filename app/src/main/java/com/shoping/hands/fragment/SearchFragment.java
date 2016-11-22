2a18a4c1ecdb6e13200a33b32cd9bd776fe9103d
mingge <uu993@163.com>
2016-11-22 18:09:30 +0800

    大淘客联盟

--- app/src/main/java/com/shoping/hands/fragment/SearchFragment.java
+++ app/src/main/java/com/shoping/hands/fragment/SearchFragment.java
@@ -6,21 +6,47 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.shoping.hands.R;
 import com.shoping.hands.base.BaseFragment;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by mingge on 2016/11/8.
  */
 
 public class SearchFragment extends BaseFragment {
-    RecyclerView recyclerView;
+    @Bind(R.id.hot_recyclerview)
+    RecyclerView hotRecyclerview;
+    @Bind(R.id.history_list)
+    ListView historyList;
+    @Bind(R.id.clear_history)
+    Button clearHistory;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_search, container, false);
+        ButterKnife.bind(this, view);
         return view;
+    }
+
+    @OnClick({R.id.clear_history})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.clear_history:
+                break;
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 }
